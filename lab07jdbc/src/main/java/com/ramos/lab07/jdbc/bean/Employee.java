77aa3c2fcaca69da9b9549c067f2683764109576
Rodrigo <exit>
2017-09-27 12:06:05 -0500

    primera subida

---
+++ lab07jdbc/src/main/java/com/ramos/lab07/jdbc/bean/Employee.java
@@ -0,0 +1,25 @@
+package com.ramos.lab07.jdbc.bean;
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	String firstName;
+	String lastName;
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
